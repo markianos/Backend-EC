c9591e3e66d8c3419a85ffdc9bf3fdfdf4fd5fc9
Alexander Markianos <markianos.alexander@gmail.com>
2021-11-21 23:13:01 +0100

    inlämning 1& 2
    Backend för ECutbildning - inlämningsuppgift 1 & 2

---
+++ WebApp_new/Data/SqlContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp_new.Models;
+
+namespace WebApp_new.Data
+{
+    public class SqlContext : DbContext
+    {
+        public SqlContext(DbContextOptions<SqlContext> options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WebApp_new/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp_new.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        [Column(TypeName = "nvarchar(200)")]
+        public string Name { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(max)")]
+        public string Description { get; set; }
+
+        [Required]
+        [Column(TypeName = "money")]
+        [DisplayFormat(DataFormatString = "{0:n0}")]
+        public decimal Price { get; set; }
+
+    }
+}
